4e94f78c9a3e11d57de20cae301a13b320777d16
Ryan Villanueva <xxryan0909@gmail.com>
2020-12-25 14:52:14 +0800

    new Update

--- app/src/main/java/xyz/paypnt/paypoint/Dashboard.java
+++ app/src/main/java/xyz/paypnt/paypoint/Dashboard.java
@@ -1,22 +1,42 @@
 package xyz.paypnt.paypoint;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 
 import com.example.paypoint.R;
 
 public class Dashboard extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
         setContentView(R.layout.dashboard);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerButton);
+        actionBarDrawerToggle = new ActionBarDrawerToggle( this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void logout(View view) {
