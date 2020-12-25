e4e3db68eef53e118f0d27e1aaee0fe09399ded3
Ryan Villanueva <xxryan0909@gmail.com>
2020-12-25 18:22:39 +0800

    new Update

--- app/src/main/java/xyz/paypnt/paypoint/Dashboard.java
+++ app/src/main/java/xyz/paypnt/paypoint/Dashboard.java
@@ -1,7 +1,6 @@
 package xyz.paypnt.paypoint;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,12 +10,12 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.ActionMenuView;
 import android.widget.Toast;
 
 import com.example.paypoint.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -27,10 +26,54 @@ public class Dashboard extends AppCompatActivity {
 //        getSupportActionBar().hide();
         setContentView(R.layout.dashboard);
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawerButton);
-        actionBarDrawerToggle = new ActionBarDrawerToggle( this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerButton);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void logout(View view) {
+        startActivity(new Intent(Dashboard.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_dashboard:
+                System.out.println("Dashboard");
+                break;
+            case R.id.action_history:
+                System.out.println("History");
+                break;
+            case R.id.action_password:
+                System.out.println("Password");
+                break;
+            case R.id.action_use:
+                System.out.println("How to Use?");
+                break;
+            case R.id.action_aboutUs:
+                System.out.println("About Us");
+                break;
+            case R.id.action_logOut:
+                System.out.println("Log Out");
+                break;
+
+        }
+        return true;
     }
 }
