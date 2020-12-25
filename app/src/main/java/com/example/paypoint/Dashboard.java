3cf29321168c3338684a141bbf7feaf6ae34ec9e
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 12:42:27 +0800

    Added Dashboard

---
+++ app/src/main/java/com/example/paypoint/Dashboard.java
@@ -0,0 +1,24 @@
+package com.example.paypoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.dashboard);
+    }
+
+    public void logout(View view) {
+        startActivity(new Intent(Dashboard.this, MainActivity.class));
+        finish();
+    }
+}
