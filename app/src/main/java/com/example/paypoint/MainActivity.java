4d59cfa5852a13ff574a88b43445a79c7c05582a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-25 11:38:36 +0800

    Splashscreen - done; Main Activity - Ongoing

--- app/src/main/java/com/example/paypoint/MainActivity.java
+++ app/src/main/java/com/example/paypoint/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.paypoint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/paypoint/Splashscreen.java
@@ -0,0 +1,32 @@
+package com.example.paypoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class Splashscreen extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.splashscreen);
+
+        //Delay for splashscreen
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },500);
+    }
+}
